8053f6bfc404b44a3a4a788f9df1bfb3ddf4e98f
bernhardtjj <jbernhardt@fwparker.org>
2017-02-02 18:44:36 -0600

    Homework Completed

--- Conditions.py
+++ Conditions.py
@@ -1,4 +1,5 @@
-#CONDITIONS (15PTS TOTAL)
+# CONDITIONS (15PTS TOTAL)
+from math import sqrt
 
 # PROBLEM 1 (GPA - 4pts)
 # Grades are values between 0 and 100
@@ -8,22 +9,64 @@
 # Make a series of if/elif/else statements to print the letter grade.
 # If the user enters a grade lower than zero or higher than 100, just give an error message.
 # Don't worry about making an exception for these right now.
+grade = int(float(input('Give me a percentage grade: ')))
+if grade > 100:
+    print("Error, that's not a grade.")
+elif 97 <= grade <= 100:
+    print("A+")
+elif 93 <= grade <= 96:
+    print("A")
+elif 90 <= grade <= 92:
+    print("A-")
+elif 87 <= grade <= 89:
+    print("B+")
+elif 83 <= grade <= 86:
+    print("B")
+elif 80 <= grade <= 82:
+    print("B-")
+elif 77 <= grade <= 79:
+    print("C+")
+elif 73 <= grade <= 76:
+    print("C")
+elif 70 <= grade <= 72:
+    print("C-")
+elif 67 <= grade <= 69:
+    print("D+")
+elif 65 <= grade <= 66:
+    print("D")
+elif 0 <= grade <= 64:
+    print("F")
+elif grade < 0:
+    print("Error, that's not a grade.")
 
 # PROBLEM 2 (Vowels - 5pts)
 # Ask the user to supply a string.
-# Print how many different vowels there are in the string.
+# Print how many
+'''different'''  # vowels there are in the string.
 # The capital version of a lower case vowel is considered to be the same vowel.
 # y is not considered a vowel.
 # Try to print proper output (e.g., printing “There are 1 different vowels in the string” is ugly).
 # Example: When the user enters the string “It’s Owl Stretching Time,”
 # the program should say that there are 3 different vowels in the string.
-
+string = input("Supply a string: ").lower()
+vowels = 0
+for i in ['a', 'e', 'i', 'o', 'u']:
+    if i in string:
+        vowels += 1
+if vowels == 1:
+    print("There is a vowel in the string.")
+elif vowels:
+    print("There are", vowels, "different vowels in the string.")
+else:
+    print("There aren't any vowels in the string.")
 # PROBLEM 3 (Quadratic Equation - 6pts)
-# You can solve quadratic equations using the quadratic formula.
-# Quadratic equations are of the form Ax2 + Bx + C = 0.
+print('''I can solve quadratic equations using the quadratic formula.
+Quadratic equations are of the form Ax^2 + Bx + C = 0.''')
+A = float(input("A= "))
+B = float(input("B= "))
+C = float(input("C= "))
 # Such equations have zero, one or two solutions.
-# The first solution is (−B + sqrt(B^22 − 4AC))/(2A).
-# The second solution is (−B − sqrt(B^2 − 4AC))/(2A).
+
 # There are no solutions if the value under the square root is negative.
 # There is one solution if the value under the square root is zero.
 # Write a program that asks the user for the values of A, B, and C,
@@ -31,3 +74,16 @@
 # then prints those solutions.
 # Note: Make sure that you also take into account the case that A is zero,
 # and the case that both A and B are zero.
+if not A or not B:
+    print("A, B have to be nonzero.")
+    quit()
+if B ** 2 - 4 * A * C < 0:
+    print("There are no solutions.")
+    quit()
+first_solution = (-1 * B + sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+second_solution = (-1 * B - sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+if B ** 2 - 4 * A * C == 0:
+    print("x =", first_solution)
+else:
+    print("x1 =", first_solution)
+    print("x2 =", second_solution)
--- Lists.py
+++ Lists.py
@@ -1,41 +1,67 @@
-#LISTS (35PTS TOTAL)
-#In these exercises you write functions. Of course, you should not only write the functions,
-#you should also write code to test them. For practice, you should also comment your
-#functions as explained above.
+import random
+
+# LISTS (35PTS TOTAL)
+# In these exercises you write functions. Of course, you should not only write the functions,
+# you should also write code to test them. For practice, you should also comment your
+# functions as explained above.
 
 
-
-#PROBLEM 1 (8-ball - 5pts)
+# PROBLEM 1 (8-ball - 5pts)
 # A magic 8-ball, when asked a question, provides a random answer from a list.
 # The code below contains a list of possible answers. Create a magic 8-ball program that
 # prints a random answer.
-answer_list = [ "It is certain", "It is decidedly so", "Without a \
-doubt", "Yes, definitely", "You may rely on it", "As I see it, \
-yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
-"Reply hazy try again", "Ask again later", "Better not tell you \
-now", "Cannot predict now", "Concentrate and ask again", "Don ' t \
-count on it", "My reply is no", "My sources say no", "Outlook \
-not so good", "Very doubtful" ]
+answer_list = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it",
+               "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+               "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+               "Don ' t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-
+input("What is your question? ")
+print(answer_list.pop(random.randrange(len(answer_list))))
+print()
 # PROBLEM 2 (Shuffle - 5pts)
 # A playing card consists of a suit (Heart, Diamond, Club, Spade) and a value (2,3,4,5,6,7,8,9,10,J,Q,K,A).
 # Create a list of all possible playing cards, which is a deck.
 # Then create a function that shuffles the deck, producing a random order.
+card_values = ('Heart', 'Diamond', 'Club', 'Spade')
+card_suits = (2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A')
 
+sorted_deck = [(num, suit) for num in card_values for suit in card_suits]
+# get it, cards are tuples because you can't change their names
+# and suits, but the deck is a list because you can change that
+print(sorted_deck)
+
+shuffled_deck = []
+while sorted_deck:
+    shuffled_deck.append(sorted_deck.pop(random.randrange(len(sorted_deck))))
+
+print(shuffled_deck)
 
 # PROBLEM 3 (The sieve of Eratosthenes - 10pts)
-# The sieve of Eratosthenes is a method to find all prime numbers between
-# 1 and a given number using a list. This works as follows: Fill the list with the sequence of
+print("""
+ The sieve of Eratosthenes is a method to find all prime numbers between
+ 1 and a given number using a list. This works very well.""")
+# Fill the list with the sequence of
 # numbers from 1 to the highest number. Set the value of 1 to zero, as 1 is not prime.
-# Now loop over the list. Find the next number on the list that is not zero,
-# which, at the start, is the number 2. Now set all multiples of this number to zero.
-# Then find the next number on the list that is not zero, which is 3.
-# Set all multiples of this number to zero. Then the next number, which is 5
-# (because 4 has already been set to zero), and do the same thing again.
+sieve = [i for i in range(1, 1001)]
+sieve[0] = 0
+# Now loop over the list.
+for num in sieve:
+    # Find the next number on the list that is not zero, which, at the start, is the number 2.
+    if not num:
+        continue
+    # Now set all multiples of this number to zero.
+    for i in range(len(sieve)):
+        if num != sieve[i] and not sieve[i] % num:
+            sieve[i] = 0
+            # Then find the next number on the list that is not zero, which is 3.
+            # Set all multiples of this number to zero. Then the next number, which is 5
+            # (because 4 has already been set to zero), and do the same thing again.
+
 # Process all the numbers of the list in this way. When you have finished,
 # the only numbers left on the list are primes.
 # Use this method to determine all the primes between 1 and 1000.
+
+print(" Here are the primes:", [i for i in sieve if i])
 
 # PROBLEM 4 (Tic-Tac-Toe - 15pts)
 # Write a Tic-Tac-Toe program that allows two people to play the game against each other.
@@ -55,22 +81,101 @@ not so good", "Very doubtful" ]
 # I also use a function opponent(), that takes the player as argument and returns
 # the opponent. I use that to switch players after each move.
 
-# The main program will be something along the lines of (in pseudo-code):
-# display board
-# while True:
-#   ask for row
-#   ask for column
-#       if row/column already occupied:
-#           display error
-#   place player marker in row/col
-#   display board
-#   check for winner:
-#       announce winner
-#       break
-#   check board full:
-#       announce draw
-#       break
-#   switch player
+tic_tac = [[' ' for i in range(3)] for n in range(3)]
+marker = 'O'
+
+
+def printer_function():
+    """It prints the square again and again and again."""
+    boxes = 3
+    num = 1
+    max_rows = num * boxes + boxes + 1
+    for row in range(max_rows):
+        if row % (max_rows // boxes):
+            p1 = "|"
+            p2 = tic_tac[boxes * row // max_rows]
+        else:
+            p1 = "+"
+            p2 = ["-" for b in range(boxes)]
+        for b in range(boxes):
+            print(p1, end=' ')
+            for i in range(num):
+                print(p2[b], end=" ")
+        print(p1)
+
+
+def switch_player(m):
+    if m == 'X':
+        return 'O'
+    if m == 'O':
+        return 'X'
+
+
+def check_for_winner(m):
+    for i in range(len(tic_tac)):
+        for n in range(len(tic_tac[i])):
+            if tic_tac[n][i] != m:
+                break
+        else:
+            return True
+    else:
+        for i in range(len(tic_tac)):
+            for n in range(len(tic_tac[i])):
+                if tic_tac[i][n] != m:
+                    break
+            else:
+                return True
+        else:
+            for i in range(len(tic_tac)):
+                if tic_tac[i][i] != m:
+                    break
+            else:
+                return True
+            for i in range(len(tic_tac)):
+                if tic_tac[-1 * (i + 1)][i] != m:
+                    break
+            else:
+                return True
+            return False
+
+
+def check_board_full():
+    for i in tic_tac:
+        if ' ' in i:
+            break
+    else:
+        return True
+    return False
+
+
+def announce_winner(m):
+    print("The winner is", m)
+
+
+def announce_draw():
+    print("It's a draw")
+
+
+# The main program will be something along the lines of (in pseudo-pseudo-code):
+printer_function()  # display board
+while True:  # while True:
+    row = int(input("Row = ")) - 1  # ask for row
+    column = int(input("Column = ")) - 1  # ask for column
+    if row > 2 or column > 2 or row < 0 or column < 0:
+        print("That's not a location")
+        continue
+    if tic_tac[row][column] != ' ':  # if row/column already occupied:
+        print("That space is already taken")  # display error
+        continue
+    tic_tac[row][column] = marker  # place player marker in row/col
+    printer_function()  # display board
+    if check_for_winner(marker):
+        announce_winner(marker)
+        break
+    if check_board_full():
+        announce_draw()
+        break
+    marker = switch_player(marker)
 
 # CHALLENGE PROBLEM 5 (Battleship NO CREDIT, JUST IF YOU WANT TO TRY IT)
 # Create a program that is a simplified version of the game “Battleship.”
@@ -95,3 +200,4 @@ not so good", "Very doubtful" ]
 # Once the rest of the code works, add a function that places the battleships at random,
 # at first without checking if they are touching one another.
 # Once that works, add code that disallows battleships touching each other.
+# Maybe I will do this later if I have time.
